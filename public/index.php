4431d1ac68cf36eae5f0c84e7e8c2d30ea2bc18d
DiverCesar <diver.cesar123@gmail.com>
2026-05-20 22:35:41 -0500

    force error mesagges

--- public/index.php
+++ public/index.php
@@ -6,6 +6,12 @@ declare(strict_types=1);
  * Todo el tráfico web entra por este archivo.
  */
 
+// --- MODO DEPURACIÓN FORZADA ---
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+// -------------------------------
+
 $basePath = dirname(__DIR__);
 define('BASE_PATH', $basePath);
 
